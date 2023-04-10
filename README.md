b9094c45be143edb0e79c7413cf0bca2f70a3be8
MucahidDeveloper <Mucadad221@gmail.com>
2023-04-10 16:15:27 +0300

    updating the application to become much appropriate to 'cats'

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-A small Application with React allow you to search between Neighborhood cats
+A small Application with React allow you to search between Neighborhood cats.
+
+These lovely cats you can see comes from a site called robohash.org.
+With small amount of details you can got your personal cat looking!
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React from "react";
 
 const Card = ({ name, email, id }) => {
   return (
-    <div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
-      <img alt='robots' src={`https://robohash.org/${id}?size=200x200`} />
+    <div className="tc grow bg-light-yellow br3 pa3 ma2 dib bw2 shadow-5">
+      <img
+        alt="cute cat"
+        src={`https://robohash.org/${id}?set=set4`}
+        height={200}
+        width={200}
+      />
       <div>
         <h2>{name}</h2>
         <p>{email}</p>
       </div>
     </div>
   );
-}
+};
 
 export default Card;
